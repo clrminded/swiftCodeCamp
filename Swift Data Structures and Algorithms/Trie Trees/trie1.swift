e626480fee5abd7e505a4c020a7ffdeec156f732
Christopher Liudahl-Rackley <clrminded@users.noreply.github.com>
2020-12-31 02:55:48 -0800

    finished trie

--- Swift Data Structures and Algorithms/Trie Trees/trie1.swift
+++ Swift Data Structures and Algorithms/Trie Trees/trie1.swift
@@ -16,4 +16,60 @@ public class Trie {
 	init() {
 		rootNode = TrieNode<Character>()
 	}
+	
+	func insert(word: String) {
+		guard !word.isEmpty else { return }
+		var curNode = rootNode
+		let characters = Array(word.lowercased())
+		var curIndex = 0
+		while curIndex < word.count {
+			let character = characters[curIndex]
+			if let child = curNode.children[character] {
+				curNode = child
+			} else {
+				curNode.children[character] = TrieNode(value: character, parent: curNode)
+				curNode = curNode.children[character]!
+				
+			}
+			curIndex += 1
+			if curIndex == characters.count {
+				curNode.isFinal = true
+			}
+		}
+	}
+	
+	func query(word: String) -> Bool {
+		let characters = Array(word.lowercased())
+		var node: TrieNode? = rootNode
+		for character in characters {
+			node = node?.children[character]
+			if node == nil {
+				return false
+			}
+		}
+		return node!.isFinal
+	}
+	
+	
+	func remove(word: String) {
+		let characters = Array(word.lowercased())
+		var node: TrieNode? = rootNode
+		for character in characters {
+			node = node?.children[character]
+			if node == nil {
+				return
+			}
+		}
+		return node!.isFinal = false
+	}
 }
+
+var myTrie = Trie()
+myTrie.insert(word: "code" )
+myTrie.insert(word: "comb" )
+
+print(myTrie.query(word: "code"))
+myTrie.remove(word: "code" )
+print(myTrie.query(word: "code"))
+
+
